306ae4ba9c2f9481dcddc9b3daa2fc91f89afab9
devchanki <kgichan12@gmail.com>
2018-01-11 02:17:12 +0900

    시세

--- btc-discord-bot.js
+++ btc-discord-bot.js
@@ -32,18 +32,18 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     to: channelID,
                     message: 'Pong!'  });
 
-		case 'help'
- bot.sendMessage({
- to: channelID,
- message :
- '------------������------------------------\n'
-���� �ڿ� ��ȣȭ���� ��ȣ�� �Է��Ͻø� ���Ͻô� ��ȣȭ���� ������ �� �� �ֽ��ϴ�.
-BTC(��Ʈ����), ETH(�̴�����),
-STRAT(��Ʈ��Ƽ��), OMG(���̼���),
-DASH(�뽬), LTC(����Ʈ����), EOS(�̿���) \n
-(ex: !���� BTC ���� �Է��Ͻø� ��Ʈ������ �ü��� �� �� �ֽ��ϴ�.)
+		        case 'help'
+                 bot.sendMessage({
+                   to: channelID,
+    	              message : 
+    	'------------사용법------------------------\n'
+   	가격 뒤에 암호화폐의 기호를 입력하시면 원하시는 암호화폐의 가격을 볼 수 있습니다.
+   	BTC(비트코인), ETH(이더리움), STRAT(스트라티스), OMG(오미세고),
+   	DASH(대쉬), LTC(라이트코인), EOS(이오스) \n
+   	(ex: !가격 BTC 라고 입력하시면 비트코인의 시세를 볼 수 있습니다.)
 
-});
+
+              });
 
             break;
             // Just add any case commands if you want to..
