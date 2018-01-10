7fd2d9849efe52ebc0d6d0619ca8e682ee73ebb6
devchanki <kgichan12@gmail.com>
2018-01-11 02:43:23 +0900

    2

--- btc-discord-bot.js
+++ btc-discord-bot.js
@@ -32,9 +32,14 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 	              message :
    	    "!가격 : 암호화폐의 기호를 입력하시면 원하시는 암호화폐의 가격을 볼 수 있습니다.\n\n"+
    	    "btc(비트코인), eth(이더리움), strat(스트라티스), omg(오미세고), dash(대쉬), ltc(라이트코인), eos(이오스) \n\n"+
-   	    "(ex: !가격 btc)\n\n"
+   	    "(ex: !가격 btc)\n\n" +
 
+        "!가격 : 뒤에 코인 이름을 작성하지 않으면 모든 코인의 시세를 알 수 있습니다.\n\n" +
+
+        "!알람 : 암호화폐의 지정가를 입력하면 그 시점에 도달하면 알림을 받을 수 있습니다. \n\n"
               });
+
+
 
             break;
             // Just add any case commands if you want to..
