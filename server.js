ba12e0441020ca72625aa80f522d0926ef2b70a3
devchanki <kgichan12@gmail.com>
2018-01-11 00:41:48 +0900

    rm

--- server.js
+++
@@ -1,7 +0,0 @@
-// JavaScript source code
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => {
-    res.send('hello world');
-});
-app.listen(8080, () => { console.log('Express App on port 8080!'); });
